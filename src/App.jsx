772d8cfce55f85a71e8d7ee1aadc9117717b9f5d
Evrim <evriimciftci11@gmail.com>
2024-12-09 17:54:13 -0500

    Updates

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MainLayout from "./Components/MainLayout";
-import "./index.css"; // Ensure Tailwind CSS is imported
-
-function App() {
-  return <MainLayout />;
-}
-
-export default App;
